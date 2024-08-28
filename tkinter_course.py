fa12bd402aa68c7ce06f573e9059cec05255ed3a
frank6372 <francis-masse@hotmail.com>
2024-08-28 13:28:46 -0300

    Add files via upload

---
+++ tkinter_course.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.title("My first GUI")
+root.geometry("1200x1000")
+# Creating a label widget
+myLAbel1 = Label(root, text = "Hello World!")
+myLAbel2 = Label(root, text = "My name is Frank!")
+# insert label on the grid:
+myLAbel1.grid(row=0, column=0)
+myLAbel2.grid(row=1, column=0)
+
+root.mainloop()
+
